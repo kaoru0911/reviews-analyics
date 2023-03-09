51b9527d8577516a6f3d6c4276b80a9789697bbd
Ryder Tsai <kaoru0911@gmail.com>
2023-03-09 17:00:00 +0800

    add filter

--- read.py
+++ read.py
@@ -12,3 +12,11 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('每筆留言平均長度是', sum_len / len(data), '字')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '比留言長度小於100')
+print(new[0])
+print(new[1])
