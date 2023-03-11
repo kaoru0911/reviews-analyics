7584a81f509d2af5991e61c7449a292a42b1076b
Ryder Tsai <kaoru0911@gmail.com>
2023-03-12 01:00:29 +0800

    add list comprehension

--- read.py
+++ read.py
@@ -17,6 +17,30 @@ new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print('一共有', len(new), '比留言長度小於100')
+print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+
+#  good = []
+#  for d in data:
+#  	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到good')
+# print(good[0])
+
+good = [1 for d in data if 'good' in d]
+print(good)
+
+# 'bad' in d會顯示True or False
+# bad = []
+# for d in data:
+# 	bad.append('bad in d')
+
+bad = ['bad' in d for d in data]
+print(bad)
+
+bad = [d in d for d in data]
+print(bad)
+
+
