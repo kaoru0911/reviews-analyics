8e799a9422727b44bfb456d293fbdd4ff0066760
Ryder Tsai <kaoru0911@gmail.com>
2023-03-23 00:56:22 +0800

    add word count

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ with open('reviews.txt', 'r') as f:
 	 	if count % 1000 == 0:
 	 		print(len(data))
 print('檔案讀取完畢, 總共有', len(data), '筆資料')
-			
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -22,12 +22,12 @@ print(new[0])
 print(new[1])
 
 
-#  good = []
-#  for d in data:
-#  	if 'good' in d:
-# 		good.append(d)
-# print('一共有', len(good), '筆留言提到good')
-# print(good[0])
+ good = []
+ for d in data:
+ 	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
 
 good = [1 for d in data if 'good' in d]
 print(good)
@@ -42,5 +42,31 @@ print(bad)
 
 bad = [d in d for d in data]
 print(bad)
+
+
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+while True:
+	word = input('請問你想查什麼字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的字數為', wc[word])
+	else:
+		print('這個字沒有出現過喔！')
+
+print('感謝使用本查詢功能!')
 
 
